5ba3ad7e73e0f56f8df1468910a43803a0024000
Graham Reeves <graham@grahamreeves.com>
2024-01-07 17:00:39 +0000

    Added universal framework target & build config/script

--- Source_Apple/BuildUniversalFramework.sh
+++ Source_Apple/BuildUniversalFramework.sh
@@ -11,25 +11,36 @@ BUILD_UNIVERSAL_DIR=${BUILT_PRODUCTS_DIR}
 
 BUILDPATH_IOS="${BUILD_DIR}/${PROJECT_NAME}_Ios"
 BUILDPATH_SIM="${BUILD_DIR}/${PROJECT_NAME}_IosSimulator"
-BUILDPATH_OSX="${BUILD_DIR}/${PROJECT_NAME}_Osx"
+#BUILDPATH_MACOS="${BUILD_DIR}/${PROJECT_NAME}_Osx"
+BUILDPATH_MACOS="${BUILD_DIR}/${PROJECT_NAME}_Macos"
 BUILDPATH_TVOS="${BUILD_DIR}/${PROJECT_NAME}_Tvos"
+SCHEME_MACOS="${PROJECT_NAME}_Framework"
 
 PRODUCT_NAME_UNIVERSAL="${PROJECT_NAME}.xcframework"
+PRODUCT_PATH="${BUILD_UNIVERSAL_DIR}/${PRODUCT_NAME_UNIVERSAL}"
 
 CONFIGURATION="Release"
 DESTINATION_IOS="generic/platform=iOS"
+DESTINATION_MACOS="generic/platform=macos"
 
 # build archived frameworks
 # see https://github.com/NewChromantics/PopAction_BuildApple/blob/master/index.js for some battle tested CLI options
 echo "Building sub-framework archives..."
-xcodebuild archive -scheme ${PROJECT_NAME}_Ios -archivePath $BUILDPATH_IOS SKIP_INSTALL=NO -sdk iphoneos -configuration ${CONFIGURATION} -destination ${DESTINATION_IOS}
+#xcodebuild archive -scheme ${PROJECT_NAME}_Ios -archivePath $BUILDPATH_IOS SKIP_INSTALL=NO -sdk iphoneos -configuration ${CONFIGURATION} -destination ${DESTINATION_IOS}
 #xcodebuild archive -scheme ${PROJECT_NAME}_Ios -archivePath $BUILDPATH_SIM SKIP_INSTALL=NO -sdk iphonesimulator
-#xcodebuild archive -scheme ${PROJECT_NAME}_Osx -archivePath $BUILDPATH_OSX SKIP_INSTALL=NO
+xcodebuild archive -scheme ${SCHEME_MACOS} -archivePath $BUILDPATH_MACOS SKIP_INSTALL=NO -configuration ${CONFIGURATION} -destination ${DESTINATION_MACOS}
+
+
+if [ -d "${PRODUCT_PATH}" ] ; then
+	echo "Cleaning (deleting) old xcframework... (${PRODUCT_PATH})"
+	rm -rf ${PRODUCT_PATH}
+fi
 
 # bundle together
-echo "Building xcframework..."
+echo "Building xcframework... (${PRODUCT_PATH})"
 #xcodebuild -create-xcframework -framework ${BUILDPATH_IOS}.xcarchive/Products/Library/Frameworks/${PROJECT_NAME}_Ios.framework -framework ${BUILDPATH_SIM}.xcarchive/Products/Library/Frameworks/${PROJECT_NAME}_Ios.framework -framework ${BUILDPATH_OSX}.xcarchive/Products/Library/Frameworks/${PROJECT_NAME}_Osx.framework -output ./build/${FULL_PRODUCT_NAME}
-xcodebuild -create-xcframework -framework ${BUILDPATH_IOS}.xcarchive/Products/Library/Frameworks/${PROJECT_NAME}_Ios.framework -output ${BUILD_UNIVERSAL_DIR}/${PRODUCT_NAME_UNIVERSAL}
+#xcodebuild -create-xcframework -framework ${BUILDPATH_IOS}.xcarchive/Products/Library/Frameworks/${PROJECT_NAME}_Ios.framework -output ${BUILD_UNIVERSAL_DIR}/${PRODUCT_NAME_UNIVERSAL}
+xcodebuild -create-xcframework -framework ${BUILDPATH_MACOS}.xcarchive/Products/Library/Frameworks/${PROJECT_NAME}.framework -output ${BUILD_UNIVERSAL_DIR}/${PRODUCT_NAME_UNIVERSAL}
 
 echo "xcframework ${PRODUCT_NAME_UNIVERSAL} built successfully"
 
